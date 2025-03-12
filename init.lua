cb3542862a33ec6a2aa65ab5941e58c6e5c9cdb4
Birger Myhrvold <bmy@skrivebord.onmicrosoft.com>
2025-03-12 07:27:15 +0100

    Fix formatting

--- init.lua
+++ init.lua
@@ -105,7 +105,7 @@ vim.opt.number = true
 -- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = ''
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -978,3 +978,5 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+-- Custom Config Files
+require 'custom.config.formatting'
---
+++ lua/custom/config/formatting.lua
@@ -0,0 +1,4 @@
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.g.sleuth_inventory_heuristics = 0
